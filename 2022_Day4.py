12465233cd2e968910dea88da31ef02cd5c3fd9d
dancastiv <dancastiv@gmail.com>
2023-11-23 19:29:09 +0000

    think it works but advent says my answer is wrong

---
+++ 2022_Day4.py
@@ -0,0 +1,26 @@
+def find_redundancy(pairs):
+    first = []
+    second = []
+    counter = 0
+    for i in pairs:
+        first = list(map(int, i[0].split("-")))
+        second = list(map(int, i[1].split("-")))
+        if (first[0] <= second[0]) and (first[1] >= second[1]):
+            counter += 1
+        if (first[0] >= second[0]) and (first[1] <= second[1]):
+            counter += 1
+    return counter
+
+data = []
+pairs = []
+with open('2022_Day4_pairs.txt') as file:
+    for line in file:
+        line = line.strip()
+        data.append(line)
+for i in data:
+   pairs.append(i.split(","))
+
+counter = find_redundancy(pairs)
+
+
+print(counter)
